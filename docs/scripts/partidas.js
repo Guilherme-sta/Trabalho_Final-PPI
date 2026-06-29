a7975ec0d9b76a69ee7af8393a8f4517804b656e
Guilherme Alves <134057697+Guilherme-sta@users.noreply.github.com>
2026-06-29 05:24:15 -0300

    "test: simula offline"

--- docs/scripts/partidas.js
+++ docs/scripts/partidas.js
@@ -1,4 +1,4 @@
-const API_URL = "https://trabalho-final-ppi-kappa-two.vercel.app";
+const API_URL = "https://trabalho-final-ppi-kappa-two.vercel.app/";
 
 document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
